7978d75449b7361bf025946a6167aa1eba9adf42
Kaitlin Cheng <chengk2@myumanitoba.ca>
2023-06-01 17:09:51 -0500

    Set up initial test suite and folders

---
+++ app/src/test/java/comp3350/highschoolhub/tests/AllTests.java
@@ -0,0 +1,12 @@
+package comp3350.highschoolhub.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+
+})
+public class AllTests {
+
+}
