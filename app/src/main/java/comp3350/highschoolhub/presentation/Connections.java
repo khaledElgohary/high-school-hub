c9b30a9fefef399f96a0a7458db8b2f991811873
Braden Yablonski <yablonsb@myumanitoba.ca>
2023-06-01 03:42:01 +0000

    Started new Connections class for adding interaction to connections page.

---
+++ app/src/main/java/comp3350/highschoolhub/presentation/Connections.java
@@ -0,0 +1,18 @@
+package comp3350.highschoolhub.presentation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import comp3350.srsys.highschoolhub.R;
+
+public class Connections extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_connections);
+
+        
+    }
+}
