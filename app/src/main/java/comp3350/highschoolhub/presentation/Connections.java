bb79f4d8df463279bac2cfc6dd6009c110564ebf
Braden Yablonski <yablonsb@myumanitoba.ca>
2023-06-02 00:49:49 +0000

    Added override methods for the menu bar.

--- app/src/main/java/comp3350/highschoolhub/presentation/Connections.java
+++ app/src/main/java/comp3350/highschoolhub/presentation/Connections.java
@@ -2,6 +2,9 @@ package comp3350.highschoolhub.presentation;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 import comp3350.highschoolhub.R;
 
@@ -16,5 +19,18 @@ public class Connections extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_user_connections, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
     }
 }
