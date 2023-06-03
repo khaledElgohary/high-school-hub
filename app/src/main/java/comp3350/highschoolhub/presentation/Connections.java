7ae4d99e39970c458a061ed23fd5b022b8b97e3a
Braden Yablonski <yablonsb@myumanitoba.ca>
2023-06-03 02:37:50 +0000

    Added more functions to make UI more interactive.

--- app/src/main/java/comp3350/highschoolhub/presentation/Connections.java
+++ app/src/main/java/comp3350/highschoolhub/presentation/Connections.java
@@ -4,16 +4,78 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.content.Intent;
+
+import comp3350.highschoolhub.business.ConnectionsManager;
+import comp3350.highschoolhub.objects.*;
+import comp3350.highschoolhub.business.AccessUsers;
+import java.util.List;
+
 
 
 import comp3350.highschoolhub.R;
 
 public class Connections extends Activity {
 
+    private AccessUsers accessUsers;
+    private ConnectionsManager connectionsManager;
+    private List<User> connectionsList;
+    private ArrayAdapter<User> connectionsArrayAdapter;
+    private int connectionsListPosition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_connections);
+
+        accessUsers = new AccessUsers();
+        connectionsManager = new ConnectionsManager();
+
+        //connectionsList = connectionsManager.getHighSchoolConnections(accessUsers.getUsers());
+        connectionsList = accessUsers.getUsers();//Get rid of this line, once the above method is implemented.
+
+        try{
+            connectionsArrayAdapter = new ArrayAdapter<User>(this, android.R.layout.simple_list_item_activated_2, android.R.id.text1, connectionsList){
+
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent){
+                    View view = super.getView(position, convertView, parent);
+
+                    TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                    TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+                    String fullName = connectionsList.get(position).getFirstName() + " " + connectionsList.get(position).getLastName();
+
+                    text1.setText(fullName);
+                    text2.setText(connectionsList.get(position).getUserId());
+
+                    return view;
+
+                }
+            };
+
+            //Set up what happens when a list item is clicked on.
+            final ListView listView = (ListView)findViewById(R.id.userConnections);
+            listView.setAdapter(connectionsArrayAdapter);
+
+            listView.setOnItemClickListener((parent, view, position, id) -> {
+                connectionsListPosition = position;
+                selectUserAtPosition(connectionsListPosition);
+            });
+
+            //Set up what happens when the my profile button is clicked on at the bottom of the screen.
+            final Button myProfileButton = (Button)findViewById(R.id.backToMyProfile);
+        }
+        catch(final Exception e){
+            Messages.fatalError(this, e.getMessage());
+        }
     }
 
     @Override
@@ -32,5 +94,24 @@ public class Connections extends Activity {
         int id = item.getItemId();
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //This method is used to navigate to a user's profile or provide a popup to send a request.
+    public void selectUserAtPosition(int position)
+    {
+        User selected = connectionsArrayAdapter.getItem(position);
+        boolean isUserAccepted;//Keeps track if the logged in user has permission to see the selected user's profile.
+
+        //Make calls to Connections manager to determine what to do next.
+        //isUserAccepted = connectionsManager.isUserAccepted(selected);//Determine whether this user has permission to see the selected user's profile.
+
+
+    }
+
+    //This method is used to go back to a user's profile page when the button is clicked on.
+    public void backToLoginUserProfile(View v){
+        System.out.println("Button clicked.");
+        //Intent userProfileIntent = new Intent(Connections.this, Connections.class);
+        //Connections.this.startActivity(userProfileIntent);
     }
 }
