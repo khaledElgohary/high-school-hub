a0cacb8caa2afd7942f91cc966ba865b23a0c5ad
Muhammad Ahmed Asif <asifma1@myumanitoba.ca>
2023-06-04 19:00:11 -0500

    Add PasswordManagerTest, add tests to AllTests

--- app/src/test/java/comp3350/highschoolhub/tests/AllTests.java
+++ app/src/test/java/comp3350/highschoolhub/tests/AllTests.java
@@ -3,6 +3,7 @@ package comp3350.highschoolhub.tests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import comp3350.highschoolhub.tests.business.PasswordManagerTest;
 import comp3350.highschoolhub.tests.objects.UserTest;
 import comp3350.highschoolhub.tests.objects.HighSchoolTest;
 import comp3350.highschoolhub.tests.objects.RequestTest;
@@ -11,7 +12,8 @@ import comp3350.highschoolhub.tests.objects.RequestTest;
 @Suite.SuiteClasses({
         HighSchoolTest.class,
         RequestTest.class,
-        UserTest.class
+        UserTest.class,
+        PasswordManagerTest.class
 })
 public class AllTests {
 
