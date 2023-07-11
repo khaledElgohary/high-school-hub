a141c2d63787291faa2782ff10d80b73a866ac20
khaledelgohary2002 <elgohark@myumanitoba.ca>
2023-07-11 11:30:21 -0500

    User is now able to edit, and upload a new profile picture

---
+++ app/src/main/java/comp3350/highschoolhub/presentation/MyProfile.java
@@ -0,0 +1,140 @@
+package comp3350.highschoolhub.presentation;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import comp3350.highschoolhub.R;
+import comp3350.highschoolhub.business.AccessUsers;
+import comp3350.highschoolhub.business.ConnectionsManager;
+import comp3350.highschoolhub.business.IAccessUsers;
+import comp3350.highschoolhub.business.IConnectionsManager;
+import comp3350.highschoolhub.objects.User;
+
+public class MyProfile extends Activity {
+    //fetching the loggedin user
+    private final User loggedIn = AccessUsers.getLoggedInUser();
+
+    private final IConnectionsManager connectionsManager = new ConnectionsManager();
+
+    private final IAccessUsers accessUsers = new AccessUsers();
+    //creating a string which is easier since both names will be combined and placed in a placeholder
+    String name = loggedIn.getFirstName() + " " + loggedIn.getLastName();
+
+    String numberOfConnections = connectionsManager.getHighSchoolConnections(loggedIn, accessUsers.getUsers()).size() + "";
+
+    String highSchoolName = loggedIn.getHighSchool().getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+
+        Button button = findViewById(R.id.backToConnections);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showConnections();
+            }
+        });
+
+        Button socialsButton = findViewById(R.id.socialLinksButton);
+        socialsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSocials();
+            }
+        });
+
+        Button highSchoolsButton = findViewById(R.id.editHighSchoolButton);
+        highSchoolsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showHighSchools();
+            }
+        });
+
+        ImageButton editImage=findViewById(R.id.edit_photo);
+
+        editImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,3);
+            }
+        });
+
+        //displaying the user name in the UI
+        TextView textView = findViewById(R.id.user_name);
+        textView.setText(name);
+
+        //displaying the number of connections a user have in the UI
+        TextView textView1 = findViewById(R.id.number_of_connections);
+        textView1.setText(numberOfConnections);
+
+        //displaying the highschool name the user was in the UI
+        TextView textView2 = findViewById(R.id.highschool_name);
+        textView2.setText(highSchoolName);
+
+        //Displaying the marital status of the user in the UI
+        TextView textView4 = findViewById(R.id.marital_status);
+        textView4.setText(loggedIn.getMaritalStatus());
+
+        //displaying the bio of the user in the UI
+        TextView textView3 = findViewById(R.id.Bio);
+        textView3.setText(loggedIn.getBio());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(resultCode==RESULT_OK && data!=null){
+            Uri selectedImage= data.getData();
+            ImageView imageView=findViewById(R.id.profile_image);
+            imageView.setImageURI(selectedImage);
+        }
+    }
+    //this method is used to return to the connections page when the button is pressed
+    private void showConnections() {
+        Intent connections = new Intent(this, Connections.class);
+        startActivity(connections);
+    }
+
+    //this method is used to go to the page where the user's socials are.
+    private void showSocials() {
+        Intent socials = new Intent(this, Socials.class);
+        startActivity(socials);
+    }
+
+    private void showHighSchools() {
+        Intent highSchools = new Intent(this, HighSchoolList.class);
+        startActivity(highSchools);
+    }
+
+    public void checkPermission(String permission, int requestCode)
+    {
+        if (ContextCompat.checkSelfPermission(MyProfile.this, permission) == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(MyProfile.this, new String[] { permission }, requestCode);
+        }
+        else {
+            Toast.makeText(MyProfile.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/test/java/comp3350/highschoolhub/tests/objects/RequestTest.java
@@ -0,0 +1,39 @@
+package comp3350.highschoolhub.tests.objects;
+
+import comp3350.highschoolhub.objects.Request;
+import comp3350.highschoolhub.objects.User;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RequestTest {
+    @Test
+    public void testRequestCreate() {
+        System.out.println("Starting testRequestCreate");
+
+        User user1 = new User(0, "Test", "User", "Hello world!", "Single");
+        User user2 = new User(1, "Summer", "Sun", "It's hot outside!", "Single");
+        Request request = new Request(user1, user2);
+
+        assertEquals(request.getSender(), user1);
+        assertEquals(request.getRecipient(), user2);
+        assertFalse(request.getAccepted());
+
+        System.out.println("Finished testRequestCreate");
+    }
+
+    @Test
+    public void testSetAccepted() {
+        System.out.println("Starting testSetAccepted");
+
+        User user1 = new User(0, "Test", "User", "Hello world!", "Single");
+        User user2 = new User(1, "Summer", "Sun", "It's hot outside!", "Single");
+        Request request = new Request(user1, user2);
+        request.setAccepted(true);
+
+        assertTrue(request.getAccepted());
+
+        System.out.println("Finished testSetAccepted");
+    }
+}
